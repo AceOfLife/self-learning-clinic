594c79f71fe71da7ad360cd0c79f83e7d68895c3
AceOfLife <teddyogbonnaya@yahoo.com>
2017-02-09 11:10:39 +0100

    library.js update
    for number which are not divisible by 3 or 5

--- fizzBuzz/app/library.js
+++ fizzBuzz/app/library.js
@@ -6,6 +6,9 @@ console.log('Buzz');
 } else if (number % 3 === 0) {
 console.log('Fizz');
 }
+else if (number % 3 !== 0 && number % 5 !== 0) {
+console.log(number);
+}
 else {
 console.log(number);
 }}
